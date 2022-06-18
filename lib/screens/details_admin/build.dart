ba0d2088a4ef7874d30144f7e91c0b145e645b48
saelsam <saelsamuelrude@gmail.com>
2022-06-19 07:28:52 +0800

    Create build.dart

---
+++ lib/screens/details_admin/build.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:kebab_ned/constants.dart';
+import 'package:kebab_ned/models/Product.dart';
+import 'package:kebab_ned/screens/details_admin/beli.dart';
+
+import 'package:kebab_ned/screens/details_admin/descriptions.dart';
+import 'package:kebab_ned/screens/details_admin/image_product.dart';
+import 'package:kebab_ned/screens/details_admin/stock.dart';
+
+class Build extends StatelessWidget {
+  final Product product;
+
+  const Build({Key? key, required this.product}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: size.height,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: size.height * 0.3),
+                  padding: EdgeInsets.only(
+                    top: size.height * 0.15,
+                    left: kDefaultPaddin,
+                    right: kDefaultPaddin,
+                  ),
+                  decoration: const BoxDecoration(
+                    color: kTextLightColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      topRight: Radius.circular(25),
+                    ),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      stock(product: product),
+                      const SizedBox(height: kDefaultPaddin / 2),
+                      description_admin(product: product),
+                      const SizedBox(height: kDefaultPaddin / 2),
+                      beli_admin(product: product),
+                      const SizedBox(height: kDefaultPaddin / 2),
+                      //     AddToCart(product: product)
+                    ],
+                  ),
+                ),
+                image(product: product)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
