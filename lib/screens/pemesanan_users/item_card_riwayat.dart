42a5d4b92c6ed9a646877bd94897422885c243f8
saelsam <saelsamuelrude@gmail.com>
2022-06-19 07:36:10 +0800

    Create item_card_riwayat.dart

---
+++ lib/screens/pemesanan_users/item_card_riwayat.dart
@@ -0,0 +1,120 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:kebab_ned/constants.dart';
+import 'package:kebab_ned/controllers/editpesanan_controller.dart';
+import 'package:kebab_ned/models/Product.dart';
+import 'package:kebab_ned/screens/menu_home_users/home.dart';
+import 'package:get/get.dart';
+
+class ItemCard extends StatelessWidget {
+  final String nama;
+  final String alamat;
+  final String kebab;
+  final int beli;
+  final int harga;
+  final String Nohp;
+  final String bayar;
+
+  //// Pointer to Update Function
+  final Function? onUpdate;
+  //// Pointer to Delete Function
+  final Function? onDelete;
+
+  ItemCard(this.nama, this.alamat, this.kebab, this.beli, this.harga, this.Nohp,
+      this.bayar,
+      {this.onUpdate, this.onDelete});
+
+  @override
+  Widget build(BuildContext context) {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference pesanan = firestore.collection('pesanan');
+    kBackgroundColor;
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          padding: const EdgeInsets.all(5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: kTextLightColor)),
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: Text(
+                        'Nama : $nama',
+                        style: TextStyle(
+                          fontWeight: reguler,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Alamat : $alamat ',
+                      style: TextStyle(
+                        fontWeight: reguler,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Kebab : $kebab',
+                      style: TextStyle(
+                        fontWeight: reguler,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Jumlah : ${beli.toInt()}',
+                      style: TextStyle(
+                        fontWeight: reguler,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Total Harga : $harga',
+                      style: TextStyle(
+                        fontWeight: reguler,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'No Hp : $Nohp',
+                      style: TextStyle(
+                        fontWeight: reguler,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Pembayaran : $bayar',
+                      style: TextStyle(
+                        fontWeight: reguler,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                ),
+              ],
+            ),
+          ]),
+        ),
+      ],
+    );
+  }
+}
+
+final EditPesananController ep = Get.put(EditPesananController());
