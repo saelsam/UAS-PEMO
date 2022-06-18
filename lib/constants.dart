b40d6c5f640b3fbb87c73ecd82ee3989ee6a2a39
saelsam <saelsamuelrude@gmail.com>
2022-06-19 07:27:01 +0800

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kTextColor = Color.fromARGB(255, 255, 174, 0);
+const kTextLightColor = Colors.blue;
+const kBackgroundColor = Color.fromARGB(255, 255, 241, 199);
+
+const kDefaultPaddin = 20.0;
+
+const bold = FontWeight.w700;
+const light = FontWeight.w300;
+const reguler = FontWeight.w400;
