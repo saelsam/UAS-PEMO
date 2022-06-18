542bad73e020a89110b603e003c7fbf85249d059
saelsam <saelsamuelrude@gmail.com>
2022-06-19 07:30:11 +0800

    Create home.dart

---
+++ lib/screens/menu_home_users/home.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:kebab_ned/constants.dart';
+import 'package:kebab_ned/controllers/users_controlllers.dart';
+import 'package:kebab_ned/models/Product.dart';
+import 'package:kebab_ned/screens/menu_home_users/build.dart';
+import 'package:get/get.dart';
+import 'package:kebab_ned/screens/registrasi.dart';
+
+class HomeScreen_users extends StatelessWidget {
+  HomeScreen_users({Key? key}) : super(key: key);
+
+  final GetxUserController uc = Get.find();
+  final user = FirebaseAuth.instance.currentUser!;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Build(),
+      // bottomNavigationBar: MyMainPage(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: kBackgroundColor,
+      elevation: 0,
+      title: Text(
+        "Hello ${uc.namaLoginController.text}",
+        style: TextStyle(
+          fontSize: 17,
+          color: kTextColor,
+        ),
+      ),
+    );
+  }
+}
